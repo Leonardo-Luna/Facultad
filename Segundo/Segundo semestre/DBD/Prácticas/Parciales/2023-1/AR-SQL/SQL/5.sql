47e9dd81996b06da3dddc04ae2457617e35e6bac
Leonardo-Luna <lunaleonardo031@gmail.com>
2024-11-11 19:08:03 -0300

    2023 1era fecha SQL-5

--- Segundo/Segundo semestre/DBD/Prácticas/Parciales/2023-1/AR-SQL/SQL/5.sql
+++ Segundo/Segundo semestre/DBD/Prácticas/Parciales/2023-1/AR-SQL/SQL/5.sql
@@ -5,4 +5,9 @@ del pedido para los pedidos de octubre de 2023.
 
 **/
 
-SELECT p.nroPedido, count(distinct ra.cantRemito), 
+SELECT p.nroPedido, count(distinct pa.codigoArticulo) as cantidad, sum(pa.cantPedido * a.precio) AS montoTotal
+FROM Pedidos p
+INNER JOIN PedidoArticulo pa ON (p.nroPedido = pa.nroPedido)
+INNER JOIN Articulos a ON (pa.codigoArticulo = a.codigoArticulo)
+WHERE (p.fechaPedido BETWEEN '2023-10-1' AND '2023-10-31')
+GROUP BY p.nroPedido;
